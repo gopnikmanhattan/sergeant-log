0d74fedd8c2039bdd1ea265a6c47cb36119905c0
parmigggiana <francesco.basile12@studenti.unina.it>
2020-03-30 13:29:43 +0200

    tt

---
+++ c++/basi.cpp
@@ -0,0 +1,17 @@
+#include "basi.h"
+
+int len (char* word) {
+	int i = 0;
+	while (word[i] != '\0') {
+		i++;
+	}
+	return i;
+}
+
+void bg_rgb (int r, int g, int b) {
+	std::cout << "\u001b[48;2;" << r << ";" << g << ";" << b << "m";
+}
+
+void fg_rgb (int r, int g, int b) {
+	std::cout << "\u001b[38;2;" << r << ";" << g << ";" << b << "m";
+}
---
+++ c++/basi.h
@@ -0,0 +1,4 @@
+int len (char* word);
+void bg_rgb (int r, int g, int b);
+void fg_rgb (int r, int g, int b);
+
