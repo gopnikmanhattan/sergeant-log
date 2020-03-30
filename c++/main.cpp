727c0df462b7d435894ad8a8e3413f6a680e5f9b
dreamwhite <officialdreamwhite@gmail.com>
2020-03-30 13:16:53 +0200

    Added main.cpp file

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 
-void filler()
+int len(char string[]) {
+
+    int i = 0;
+    while ( string[i] != '\0'  ) {
+
+        i++;
+    }
+
+    return i;
+}
+
+int main() {
+
+    char nome[] = "ivan";
+    std::cout << len(nome);
+    return 0;
+}
