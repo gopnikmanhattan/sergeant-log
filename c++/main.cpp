aa8e788f93a15fbde31d8746fce9270b2389afbe
dreamwhite <officialdreamwhite@gmail.com>
2020-03-30 13:29:06 +0200

    Added bg_rgb function

--- c++/main.cpp
+++ c++/main.cpp
@@ -11,9 +11,24 @@ int len(char string[]) {
     return i;
 }
 
+void bg_rgb(int r, int g, int b)
+{
+    std::cout << "\u001b[48;2;" << r << ";" << g << ";" << b << "m";
+}
+
 int main() {
 
     char nome[] = "ivan";
     std::cout << len(nome);
+    bg_rgb(255, 255, 255);
     return 0;
 }
+
+
+
+/*def bg_rgb(r, g, b):
+    return f"\u001b[48;2;{r};{g};{b}m"
+def fg_rgb(r, g, b):
+    return f"\u001b[38;2;{r};{g};{b}m"
+
+*/
