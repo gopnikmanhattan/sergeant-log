856a3e3beab4e44477f6db154655eefa68ce4b01
dreamwhite <officialdreamwhite@gmail.com>
2020-03-30 13:45:25 +0200

    added void fg_rgb

--- c++/main.cpp
+++ c++/main.cpp
@@ -16,17 +16,13 @@ void bg_rgb(int r, int g, int b)
     std::cout << "\033[48;2;" << r << ";" << g << ";" << b << "m";
 }
 
+void fg_rgb(int r, int g, int b)
+{
+    std::cout << "\033[38;2;" << r << ";" << g << ";" << b << "m";
+}
+
 int main() {
 
     std::cout << "\033[48;2;255;20;180mCIAO";
     return 0;
 }
-
-
-
-/*def bg_rgb(r, g, b):
-    return f"\u001b[48;2;{r};{g};{b}m"
-def fg_rgb(r, g, b):
-    return f"\u001b[38;2;{r};{g};{b}m"
-
-*/
